d25485a1ad1150f261c779d4ab975f022061657f
vdragon97 <64758047+vdragon97@users.noreply.github.com>
2022-08-19 15:56:25 +0700

    Add files via upload

---
+++ intradaySearch.py
@@ -0,0 +1,70 @@
+import requests
+import string
+import random
+from requests.structures import CaseInsensitiveDict
+from datetime import datetime
+import os
+'''
+python livedragon.py inputDate inputMode
+
+inputDate: dd/mm/yyyy
+inputMode: all/ctn
+'''
+
+def intradaySearchFunction(inputDate, inputContract, inputCookie):   
+    url = "https://livedragon.vdsc.com.vn/general/intradaySearch.rv?stockCode=" + inputContract +"&boardDate=" + inputDate
+    headers = CaseInsensitiveDict()
+    headers["Content-Length"] = "0"
+    headers["Cookie"] = inputCookie
+    headers["User-agent"] = ""
+    headers["Connection"] = "keep-alive"
+    headers["Accept"] = "*/*"
+    headers["Accept-Encoding"] = "gzip, deflate, br"
+    #print(headers)
+    try:
+        resp = requests.post(url, headers=headers, timeout=10)
+    except:
+        print("intradaySearch.py: Try search false")
+        return
+    #print(resp.headers) #headers looks like that {'Content-Type': 'application/json;charset=UTF-8', 'Content-Language': 'vi-VN', 'Transfer-Encoding': 'chunked', 'Date': 'Fri, 05 Aug 2022 16:28:58 GMT'}
+    data = resp.json() #all data is a dictionary
+    '''
+    {
+        "success": true,
+        "message": "",
+        "list": [{}, {}, ...{}]
+    }
+    '''
+    diff = 0.8
+    long_cnt = 0
+    short_cnt = 0
+    total_match_vol = 0
+    list = data['list']  #get the list which named "list"
+    size = len(list) #row count of the grid
+    #print("row count = "+ str(size))
+    now = datetime.now() # current date and time
+    folderName = now.strftime("%Y%m%d")
+    isExist = os.path.exists("./" + folderName)
+    if not isExist:
+        os.makedirs(folderName)
+    fileName = now.strftime("%H%M%S") + ".txt"
+    f=open("./" + folderName + "/" + fileName,'w')
+    for x in reversed(range(size)):
+        if list[x]['TradeTime'] > "08:59:00" and list[x]['TradeTime'] < "14:30:00" and list[x]['BidPrice1'] > 0 and list[x]['MatchedPrice'] > 0 and list[x]['OfferPrice1'] > 0:
+            #f.seek(0) #get to the first position
+            output = str(list[x]['TradeTime']) + " | " + str(list[x]['BidPrice1']) + " | " + str(list[x]['MatchedPrice']) + " | " + str(list[x]['OfferPrice1'])
+            f.write(output)
+            f.write("\n")
+            if round(list[x]['BidPrice1'] - list[x - 1]['BidPrice1'], 1) >= diff and round(list[x]['BidPrice1'] - list[x]['MatchedPrice'], 1) >= diff and round(list[x]['OfferPrice1'] - list[x]['MatchedPrice'], 1) >= diff and round(list[x]['OfferPrice1'] - list[x - 1]['OfferPrice1'], 1) >= diff:
+                print(output + " | LONG")
+                long_cnt = long_cnt + 1
+            if round(list[x]['BidPrice1'] - list[x - 1]['BidPrice1'], 1) <= -diff and round(list[x]['BidPrice1'] - list[x]['MatchedPrice'], 1) <= -diff and round(list[x]['OfferPrice1'] - list[x]['MatchedPrice'], 1) <= -diff and round(list[x]['OfferPrice1'] - list[x - 1]['OfferPrice1'], 1) <= -diff:
+                print(output + " | SHORT")
+                short_cnt = short_cnt + 1
+        total_match_vol = total_match_vol + list[x]['MatchedVol'] 
+    print("Total LONG = " + str(long_cnt) + " | Total SHORT = " + str(short_cnt) + " | Total Matched Vol = " + str(total_match_vol * 10))
+    f.close()    
+    
+
+if __name__=="__main__":
+    intradaySearchFunction("05/08/2022", "VN30F2208", "")
---
+++ livedragonMain.py
@@ -0,0 +1,40 @@
+import intradayBoard
+import checkSession
+import intradaySearch
+import string
+import random
+import time
+import sys
+from datetime import datetime
+
+def callLiveDragon(checkDate, checkContract):
+    CookiePartOne = intradayBoard.accessMainPage()
+    #print("CookiePartOne = " + CookiePartOne)
+    if CookiePartOne =="exitMainPage":
+        return
+    CookiePartTwo ="; hideMarketChartCKName=0; allCustomGroupsCkName=ALL_DEFAULT_GROUP_ID%23%23%23%23%23%23%23%23CTD%3BDHG%3BDRC%3BFPT%3BHPG%3BHSG%3BKDC%3BMWG%3BNT2%3BPAC%3BPC1%3BPNJ%3BTAC%3BVCB%3BVDS%3BVGC%3BVJC%3BVNM%3B%23%23%23%23%23%23%23%23T%C3%B9y%20ch%E1%BB%8Dn; "
+    
+    CookiePartThree = checkSession.checkSessionFunction(CookiePartOne)
+    if CookiePartThree =="exitCheckSession":
+        return
+    CookiePartFour = 'RV9cd20160034=' + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(192))
+    
+    CookiePartFive = "; rv_avraaaaaaaaaaaaaaaa_session_=JKFNGBKNNJONIPPCPPAHKMDMJFHCFKIKMNGGNBKABEMPGKJMECDCHBJODECEEFNFDJCDOMNLNBEPOLCEAPIABICLKGDFHBAMFGMDHGDILAHBKANJKBMCCGAFKDKPIIEA"
+    
+    Cookie = CookiePartOne + CookiePartTwo + CookiePartThree + CookiePartFour + CookiePartFive
+    #print (Cookie)
+    workingTime = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")    
+    print ("--------workingTime = " + workingTime + "--------")
+    print ("checkDate = " + checkDate + " checkContract = " + checkContract)
+    intradaySearch.intradaySearchFunction(checkDate, checkContract, Cookie)
+    
+if __name__=="__main__":    
+    while(True):
+        if (len(sys.argv) > 1):
+            callLiveDragon(sys.argv[1], sys.argv[2])
+        else: 
+            #print(datetime.now().strftime("%d/%m/%Y"))
+            #print("VN30F" + datetime.now().strftime("%Y")[2:4] + datetime.now().strftime("%m"))
+            callLiveDragon(datetime.now().strftime("%d/%m/%Y"), "VN30F" + datetime.now().strftime("%Y")[2:4] + datetime.now().strftime("%m"))
+            print("--------------------------------------------------")
+        time.sleep(10)
